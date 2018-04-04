39f18023df3e92f906aec683687c25105e7029f1
rbeltransa <35699743+rbeltransa@users.noreply.github.com>
2018-04-04 19:49:49 +0200

    Update appiCopia.java

--- PremierURJC/src/main/java/com/example/demo/appiCopia.java
+++ PremierURJC/src/main/java/com/example/demo/appiCopia.java
@@ -1,1 +1,11 @@
+package com.example.demo;
+import java.util.List;
 
+@RestController
+public class appiCopia {
+
+	private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+	
+	@Autowired
+	private RepositorioJugador repositorio_de_jugadores;
+  
