babb05ba4edf678902e4def221c6d4b07cfda88e
fsanchezgar <35699681+fsanchezgar@users.noreply.github.com>
2018-04-04 20:13:32 +0200

    Update appiCopia.java

--- PremierURJC/src/main/java/com/example/demo/appiCopia.java
+++ PremierURJC/src/main/java/com/example/demo/appiCopia.java
@@ -20,6 +20,22 @@ public class appiCopia {
 	
 	public static void salvarDatos() {
 		ArrayList<Jugador> copiaJugador = null;
+		try {
+			copiaJugador = JSON_MAPPER.readValue(new URL("http://127.0.0.1:8443/pedirDatos"), ArrayList.class);
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print("LOOOOONGITUD: "+copiaJugador.size());
 		
 	}
 }
